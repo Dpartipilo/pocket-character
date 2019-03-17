7425b64a372868d2afc2efd847ad3fbc76d1c050
Dpartipilo <Dpartipilo@hotmail.com>
2019-03-17 16:23:30 +0000

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Interface from './components/Interface'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        TEST
+        <Interface />
       </div>
     );
   }
