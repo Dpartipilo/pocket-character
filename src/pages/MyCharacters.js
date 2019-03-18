2709e650421ac2e9019b775bae859662a9afed00
Dpartipilo <Dpartipilo@hotmail.com>
2019-03-18 20:19:59 +0000

    Created pages folder with Home, About and MyCharacter pages

---
+++ src/pages/MyCharacters.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import CharacterList from '../components/CharacterList';
+
+const MyCharacters = () => (
+  <div>
+    <h1>My characters</h1>
+    <CharacterList />
+  </div>
+);
+
+export default MyCharacters;
