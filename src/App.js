5577f56eab0518fb59854eb38a4c3fa3b0c4df9e
Dpartipilo <Dpartipilo@hotmail.com>
2019-03-18 20:36:05 +0000

    imported Footer component to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import './style/App.css';
 
 class App extends Component {
