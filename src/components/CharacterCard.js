1ca5e277fbe372f0bcb1f9299d2eaba84e0bed87
Dpartipilo <Dpartipilo@hotmail.com>
2019-03-21 19:32:59 +0000

    Made CharacterCards a link to profile page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 const CharacterCard = (props) => (
-  <div className="Tile">
-    <div className="avatar-container">
-      Avatar image
+  <Link to={`/clones/${props.TileId}/${props.id}/profile`}>
+    <div className="Tile">
+      <div className="avatar-container">
+        Avatar image
   </div>
-    <div className="character-details">
-      <p>{props.name}</p>
-      <p>Level:{props.level}</p>
+      <div className="character-details">
+        <p>{props.name}</p>
+        <p>Level:{props.level}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 )
 
 export default CharacterCard;
