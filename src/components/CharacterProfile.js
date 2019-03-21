904ac339118969c46fd2e3448c04c02992659ff3
Dpartipilo <Dpartipilo@hotmail.com>
2019-03-21 19:34:50 +0000

    Created CharacterProfile.js

---
+++ src/components/CharacterProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/CharacterProfile.css';
+
+
+const CharacterProfile = (props) => (
+  <div className="CharacterProfile">
+    Hello profile!
+
+  </div>
+);
+
+export default CharacterProfile;
